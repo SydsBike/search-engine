2b440505c349daa9fb4fb06ad4d84149e2c1f17c
Alex <sydsbike64@gmail.com>
2026-08-01 14:17:00 -0500

    Follow redirect chain manually and record each hop

--- src/fetch.ts
+++ src/fetch.ts
@@ -1,12 +1,25 @@
 const urlToFetch = "http://news.ycombinator.com";
 
 async function generateResponse(url: string) {
+  const request = new Request(url, { redirect: "manual" });
+
   try {
-    const response = await fetch(url);
-    if (!response.ok) {
-      throw new Error(`Response status: ${response.status}`);
+    let response = await fetch(request);
+    let status: number = response.status;
+    let location = response.headers.get("location");
+    const responses: Response[] = [];
+    responses.push(response);
+    if (status >= 300 && status <= 400) {
+      while (status >= 300 && status < 400) {
+        if (typeof location === "string") {
+          response = await fetch(location, { redirect: "manual" });
+          responses.push(response);
+          status = response.status;
+          location = response.headers.get("location");
+        }
+      }
     }
-    console.log(response);
+    console.log(responses);
   } catch (error) {
     if (error instanceof Error) {
       console.log(error.message);
